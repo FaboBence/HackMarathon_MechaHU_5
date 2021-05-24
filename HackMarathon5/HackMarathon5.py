2f1f3960e1038b1ba41e532c6cb30dcb24709584
FaboBence <65888378+FaboBence@users.noreply.github.com>
2021-05-24 14:42:24 +0200

    recursive

--- HackMarathon5/HackMarathon5.py
+++ HackMarathon5/HackMarathon5.py
@@ -10,6 +10,10 @@ def attacher(words_in_a_list):
 				tmp += words_in_a_list[i][j:]
 	return tmp
 
+def recursive(dictionary,list_of_words,last_word):
+	for word in dictionary[last_word]:
+		recursive(list_of_words.append(word))
+
 words = ["rush", "writer", "grate", "ignorant", "cloudy", "chicken", "illness", "useless", "challenge", "comfortable",
 		 "noxious", "desk", "shade", "error", "great", "zonked", "flagrant", "cute", "plan", "daughter", "dare", "giraffe", 
 		 "airplane", "aunt", "men", "blood", "vase", "cheap", "obsolete", "tomatoes", "receipt", "festive", "screeching",
@@ -27,6 +31,7 @@ def create_path(word, dictionary):
 attached = []
 kiir = []
 dictionary = {}
+
 #init dict
 for word in words:
 	hova=[]
@@ -35,20 +40,22 @@ for word in words:
 			for j in range(1,1+min(len(i),len(word))): # j: hány betűt nézünk meg
 				if word[-j:] == i[:j]:
 					hova.append(i)
-				break
+					break
 	dictionary[word]=hova
 for kiiras in dictionary:
 	print(kiiras, dictionary[kiiras])
 
 #print(create_path(words[2],dictionary))
 	
-"""
+
 for i,firstword in enumerate(words):
 	tmp_words = deepcopy(words) # List of words that have not been used
 	attached.append(firstword)
 	kiir.append([firstword]) # tmp
 	tmp_words.remove(firstword)
 	
+	recursive(kiir[0])
+
 	iteracio = 0
 	while len(tmp_words)>0 and iteracio+1 < len(tmp_words): # Addig fut, amíg megtaláljuk a leghosszabb "összetett" szót
 		for word in tmp_words:
@@ -69,4 +76,3 @@ for i,firstword in enumerate(words):
 # Kiiratás
 for i,word in enumerate(kiir):
 	print(len(word), attacher(kiir[i]))
-"""		
