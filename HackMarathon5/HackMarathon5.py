6ba3da799f597e66ca43cbb22f20aa532fda22cd
FaboBence <65888378+FaboBence@users.noreply.github.com>
2021-05-24 13:55:16 +0200

    labirintus bejárás

--- HackMarathon5/HackMarathon5.py
+++ HackMarathon5/HackMarathon5.py
@@ -2,6 +2,14 @@ from copy import deepcopy
 
 # Closed part
 
+def attacher(words_in_a_list):
+	tmp = words_in_a_list[0]
+	for i in range(1,len(words_in_a_list)):
+		for j in range(1,1+min(len(tmp),len(words_in_a_list[i]))):
+			if tmp[-j:] == words_in_a_list[i][:j]:
+				tmp += words_in_a_list[i][j:]
+	return tmp
+
 words = ["rush", "writer", "grate", "ignorant", "cloudy", "chicken", "illness", "useless", "challenge", "comfortable",
 		 "noxious", "desk", "shade", "error", "great", "zonked", "flagrant", "cute", "plan", "daughter", "dare", "giraffe", 
 		 "airplane", "aunt", "men", "blood", "vase", "cheap", "obsolete", "tomatoes", "receipt", "festive", "screeching",
@@ -9,10 +17,12 @@ words = ["rush", "writer", "grate", "ignorant", "cloudy", "chicken", "illness", 
 
 attached = []
 kiir = []
+dictionary = {"", []}
+
 for i,firstword in enumerate(words):
 	tmp_words = deepcopy(words) # List of words that have not been used
 	attached.append(firstword)
-	kiir.append(firstword) # tmp
+	kiir.append([firstword]) # tmp
 	tmp_words.remove(firstword)
 	
 	iteracio = 0
@@ -22,7 +32,7 @@ for i,firstword in enumerate(words):
 			for j in range(1,1+min(len(attached[i]),len(word))): # j: hány betűt nézünk meg
 				if attached[i][-j:] == word[:j]:
 					attached[i] += word[j:]
-					kiir[i] += ' ' + word # tmp
+					kiir[i].append(word) # tmp
 					tmp_words.remove(word)
 					found = True
 					break
@@ -33,6 +43,6 @@ for i,firstword in enumerate(words):
 			iteracio = 0
 
 # Kiiratás
-for word in kiir:
-	print(word.count(" ")+1, word)
+for i,word in enumerate(kiir):
+	print(len(word), attacher(kiir[i]))
 		
