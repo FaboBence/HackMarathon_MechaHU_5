2d1771c44e5f8e387323fa6d6e159908ca50ba79
csgellert <csapodi.gellert@gmail.com>
2021-05-24 14:25:30 +0200

    add dict

--- HackMarathon5/HackMarathon5.py
+++ HackMarathon5/HackMarathon5.py
@@ -14,11 +14,35 @@ words = ["rush", "writer", "grate", "ignorant", "cloudy", "chicken", "illness", 
 		 "noxious", "desk", "shade", "error", "great", "zonked", "flagrant", "cute", "plan", "daughter", "dare", "giraffe", 
 		 "airplane", "aunt", "men", "blood", "vase", "cheap", "obsolete", "tomatoes", "receipt", "festive", "screeching",
 		 "moor", "ingredients", "great", "skill", "us", "expansion", "rex", "lesson", "one", "nemo", "sack"]
+def create_path(word, dictionary):
+	lista = [word]
+	puffer=[dictionary[word]]
+	while True:
+		if len(dictionary[lista[-1]])>0 and dictionary[lista[-1]][0] not in lista:
+			lista.append(dictionary[lista[-1]][0])
+		else:
+			break
+	return lista
 
 attached = []
 kiir = []
-dictionary = {"", []}
+dictionary = {}
+#init dict
+for word in words:
+	hova=[]
+	for i in words:
+		if word != i:
+			for j in range(1,1+min(len(i),len(word))): # j: hány betűt nézünk meg
+				if word[-j:] == i[:j]:
+					hova.append(i)
+				break
+	dictionary[word]=hova
+for kiiras in dictionary:
+	print(kiiras, dictionary[kiiras])
 
+#print(create_path(words[2],dictionary))
+	
+"""
 for i,firstword in enumerate(words):
 	tmp_words = deepcopy(words) # List of words that have not been used
 	attached.append(firstword)
@@ -45,4 +69,4 @@ for i,firstword in enumerate(words):
 # Kiiratás
 for i,word in enumerate(kiir):
 	print(len(word), attacher(kiir[i]))
-		
+"""		
