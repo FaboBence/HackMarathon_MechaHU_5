b420f73ab30f04c6c3fe5ffcc43cc02319177110
FaboBence <65888378+FaboBence@users.noreply.github.com>
2021-06-02 11:36:16 +0200

    Azure API integration

---
+++ HackMarathon5/API_connections.py
@@ -0,0 +1,6 @@
+import requests
+
+from azure.cognitiveservices.vision.face import FaceClient
+from azure.cognitiveservices.vision.face.models import FaceAttributeType
+from msrest.authentication import CognitiveServicesCredentials
+
