a5fa09f059f214ea8f01459b1f8f0b939e7f7a70
unknown <mihalymakovsky2000@gmail.com>
2021-06-03 12:58:19 +0200

    imp

--- HackMarathon5/Main.py
+++ HackMarathon5/Main.py
@@ -4,5 +4,5 @@ import Quote_API
 
 if __name__ == '__main__':
 	emotion = Microsoft_Face_API.Emotion_recognition()
-	Spotify_API.Spotify_Search(emotion)
-	print(Quote_API.random_quote(emotion))
+	#Spotify_API.Spotify_Search(emotion)
+	Quote_API.random_quote(emotion)
--- HackMarathon5/Quote_API.py
+++ HackMarathon5/Quote_API.py
@@ -6,6 +6,8 @@ def random_quote(emotion):
     result=quote(search, limit=50)
     short_quotes=[]
     for item in result:
-        if len(item["quote"])<100:
+        if len(item["quote"])<150:
             short_quotes.append(item)
-    return short_quotes[random.randint(0,len(short_quotes)-1)]
+    if len(short_quotes)==0:
+        print("No quote found, sry")
+    else print("\n",short_quotes[random.randint(0,len(short_quotes)-1)]['quote'], "  (by ",short_quotes[random.randint(0,len(short_quotes)-1)]["author"],")\n")
