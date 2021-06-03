9b22f79c10aa81a683baf76a4ca03c5448a480ce
unknown <mihalymakovsky2000@gmail.com>
2021-06-03 12:34:38 +0200

    rename

---
+++ HackMarathon5/Quote_API.py
@@ -0,0 +1,12 @@
+from quote import quote
+import random
+
+def random_quote(emotion):
+    search=emotion
+    result=quote(search, limit=50)
+    short_quotes=[]
+    for item in result:
+        if len(item["quote"])<100:
+            short_quotes.add(item)
+    return short_quotes[random.randint(0,len(short_quotes)-1)]
+print(random_quote("anger"))
