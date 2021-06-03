27a04c39c8483dcbdd2f2b5ac8aa97fee4dfce1d
unknown <mihalymakovsky2000@gmail.com>
2021-06-03 12:38:18 +0200

    bug fix

--- HackMarathon5/Main.py
+++ HackMarathon5/Main.py
@@ -1,6 +1,8 @@
 import Microsoft_Face_API
 import Spotify_API
+import Quote_API
 
 if __name__ == '__main__':
 	emotion = Microsoft_Face_API.Emotion_recognition()
 	Spotify_API.Spotify_Search(emotion)
+	print(Quote_API.random_quote(emotion))
--- HackMarathon5/Quote_API.py
+++ HackMarathon5/Quote_API.py
@@ -7,6 +7,6 @@ def random_quote(emotion):
     short_quotes=[]
     for item in result:
         if len(item["quote"])<100:
-            short_quotes.add(item)
+            short_quotes.append(item)
     return short_quotes[random.randint(0,len(short_quotes)-1)]
 print(random_quote("anger"))
