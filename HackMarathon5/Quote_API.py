4c23478ba9924acfbc445b0737a2f54a41372e57
FaboBence <65888378+FaboBence@users.noreply.github.com>
2021-06-03 12:42:38 +0200

    bug fix

--- HackMarathon5/Quote_API.py
+++ HackMarathon5/Quote_API.py
@@ -9,4 +9,3 @@ def random_quote(emotion):
         if len(item["quote"])<100:
             short_quotes.append(item)
     return short_quotes[random.randint(0,len(short_quotes)-1)]
-print(random_quote("anger"))
